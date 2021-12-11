d66da44adba34f692af3a9c336b146841c562099
JeanCarlosHP <jeanchpaula@gmail.com>
2021-12-11 14:02:34 -0300

    search pokemon

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 ﻿import React, { useEffect, useState } from 'react'
 import api from '../../services/api'
 
-import { Title, Input, Card, Cards } from './styles'
+import { Title, Input, Card, Cards, Error } from './styles'
 
 interface Pokemon {
   id: number
@@ -13,6 +13,8 @@ interface Pokemon {
 }
 
 const Dashboard: React.FC = () => {
+  const [newPokemon, setNewPokemon] = useState('')
+  const [inputError, setInputError] = useState('')
   const [pokemonsList, setPokemons] = useState<Pokemon[]>(() => {
     const storageRepository = localStorage.getItem('@Pokemons')
 
@@ -23,6 +25,52 @@ const Dashboard: React.FC = () => {
     fetchPokemons()
     return []
   })
+
+  const searchPokemon = async () => {
+    if (!newPokemon) {
+      setInputError('Digite um Pokemon para pesquisar')
+      return
+    }
+
+    try {
+      const response = await api.get<Pokemon>(`pokemon/${newPokemon}`)
+      const pokemon = response.data
+
+      const responseTypes = await api.get(`pokemon-form/${pokemon.id}/`)
+      const pokemonTypes = responseTypes.data
+
+      const types = []
+      for (let type of pokemonTypes.types) {
+        types.push(type.type.name)
+      }
+
+      const responseDescription = await api.get(`characteristic/${pokemon.id}/`)
+      const pokemonDescription = responseDescription.data
+
+      const responseAbilities = await api.get(`pokemon/${pokemon.id}/`)
+      const pokemonAbilities = responseAbilities.data
+
+      const abilities = []
+      for (let ability of pokemonAbilities.abilities) {
+        abilities.push(ability.ability.name)
+      }
+
+      const pokemonData = {
+        id: pokemon.id,
+        url_image: `https://cdn.traction.one/pokedex/pokemon/${pokemon.id}.png`,
+        name: pokemon.name,
+        types,
+        description: pokemonDescription.descriptions[2].description,
+        abilities
+      }
+
+      setPokemons([pokemonData, ...pokemonsList])
+      setNewPokemon('')
+    } catch (err) {
+      setInputError('Pokemon não encontrado.')
+      setNewPokemon('')
+    }
+  }
 
   async function fetchPokemons() {
     const response = await api.get('pokemon?limit=5')
@@ -77,9 +125,16 @@ const Dashboard: React.FC = () => {
       </Title>
 
       <Input>
-        <input type="text" placeholder="Pesquise um Pokemon" />
-        <button>Pesquisar</button>
+        <input
+          value={newPokemon}
+          onChange={e => setNewPokemon(e.target.value.toLowerCase())}
+          type="text"
+          placeholder="Pesquise um Pokemon"
+        />
+        <button onClick={searchPokemon}>Pesquisar</button>
       </Input>
+
+      {inputError && <Error>{inputError}</Error>}
 
       <Cards>
         {pokemonsList.map(pokemon => (
@@ -88,9 +143,7 @@ const Dashboard: React.FC = () => {
               <div className="flip-card-inner">
                 <div className="flip-card-front">
                   <img src={pokemon.url_image} alt={pokemon.name} />
-                  <h2>
-                    {pokemon.id}. {pokemon.name}
-                  </h2>
+                  <h2>{pokemon.name}</h2>
                   <p>{pokemon.types.join(' | ')}</p>
                 </div>
 
